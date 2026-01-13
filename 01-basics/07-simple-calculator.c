51b471e04f7b862364c85bd9d032bd790219d985
ByteCrafter-11 <ByteCrafter-11@users.noreply.github.com>
2026-01-13 10:30:11 +0530

    day-2: conditional programs in C

---
+++ 01-basics/07-simple-calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter operator ( + - * /): ");
+    scanf(" %c", &op);
+
+    printf("Enter teo numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (op == '+')
+        printf ("Result: %d\n", a+b);
+    else if (op == '-')
+        printf ("Result: %d\n", a-b);
+    else if (op == '*')
+        printf("Result: %d\n", a*b);
+    else if (op == '/') {
+        if (b !=0)
+            printf("Result: %d\n", a/b);
+        else    
+            printf("Division by zero is not allowed\n");
+        
+    } else
+        printf("Invalid operation\n");
+
+    return 0;
+}
