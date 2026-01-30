58d3fa438fd99a1ffa866f20d9234c9b12827d7b
ByteCrafter-11 <anishkaanandanni@gmail.com>
2026-01-30 13:33:05 +0530

    Day-14: Structures

---
+++ 10-structures/simple.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Student {
+    int roll;
+    char name;
+    float marks;
+};
+int main() {
+    struct Student s1;
+
+    printf("Enter roll number: ");
+    scanf("%d", &s1.roll);
+
+    printf("Enter name: ");
+    scanf("%s", &s1.name);
+
+    printf("Enetr marks: ");
+    scanf("%f", &s1.marks);
+
+    printf("\n--- Student Details ---\n");
+    printf("Roll: %d\n", s1.roll);
+    printf("Name: %s\n", s1.name);
+    printf("Marks: %.2f\n", s1.marks);
+
+    return 0;
+}
